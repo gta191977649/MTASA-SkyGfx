6d83cf5f4861802b86d322c591721e50deabd05c
Nurupo <191977649@qq.com>
2024-10-25 01:05:43 +1100

    fix applied to vehicle lights roatation

--- skygfx/math.lua
+++ skygfx/math.lua
@@ -69,7 +69,7 @@ function getElementSpeed(theElement, unit)
 	return (Vector3(getElementVelocity(theElement)) * mult).length
 end
 
---[[
+
 function getPositionFromElementOffset(element,offX,offY,offZ)
     local m = getElementMatrix ( element )  -- Get the matrix
     local x = offX * m[1][1] + offY * m[2][1] + offZ * m[3][1] + m[4][1]  -- Apply transform
@@ -77,19 +77,12 @@ function getPositionFromElementOffset(element,offX,offY,offZ)
     local z = offX * m[1][3] + offY * m[2][3] + offZ * m[3][3] + m[4][3]
     return x, y, z                               -- Return the transformed point
 end
---]]
+
 function findRotation( x1, y1, x2, y2 ) 
     local t = -math.deg( math.atan2( x2 - x1, y2 - y1 ) )
     return t < 0 and t + 360 or t
 end
-function getPositionFromElementOffset(element,offx,offy,offz)
-	local x,y,z = getElementPosition(element)
-	local rx,ry,rz = getElementRotation(element)
-    local rx,ry,rz = rx*degToPi,ry*degToPi,rz*degToPi
-	local rxCos,ryCos,rzCos,rxSin,rySin,rzSin = cos(rx),cos(ry),cos(rz),sin(rx),sin(ry),sin(rz)
-	m11,m12,m13,m21,m22,m23,m31,m32,m33 = rzCos*ryCos-rzSin*rxSin*rySin,ryCos*rzSin+rzCos*rxSin*rySin,-rxCos*rySin,-rxCos*rzSin,rzCos*rxCos,rxSin,rzCos*rySin+ryCos*rzSin*rxSin,rzSin*rySin-rzCos*ryCos*rxSin,rxCos*ryCos
-	return offx*m11+offy*m21+offz*m31+x,offx*m12+offy*m22+offz*m32+y,offx*m13+offy*m23+offz*m33+z
-end
+
 
 function getPositionFromOffsetByPosRot(x,y,z,rx,ry,rz,offx,offy,offz)
     local rx,ry,rz = rx*degToPi,ry*degToPi,rz*degToPi
